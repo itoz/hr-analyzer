664015efcdc8031cc11733d5e822686b4f8aa50a
itoz <romatica@gmail.com>
2019-12-25 18:38:40 +0900

    add Total componet

---
+++ src/components/Total.tsx
@@ -0,0 +1,16 @@
+import React from "react"
+
+const Total =()=>{
+    return (
+        <div>
+            人数
+            <select >
+                <option value="all">すべて</option>
+                <option value="enginieer">エンジニア</option>
+                <option value="designer">デザイナ</option>
+            </select>
+        </div>
+    )
+}
+
+export default Total
